aa5388f9a8781a330fc655833b022b7d7d3b86db
João Lucas F. Rodrigues <160081720+JLucastecsenai@users.noreply.github.com>
2024-06-28 13:04:55 -0300

    Create primeiro.kt

---
+++ primeiro.kt
@@ -0,0 +1,12 @@
+fun main(){
+ // val é uma variavel imutavel
+ // var é uma variavel mutavel
+ val priece = 31.95
+ val age = 42
+ val produto = "imac"
+
+ println(priece)
+ println(age)
+ println(produto)
+
+}
